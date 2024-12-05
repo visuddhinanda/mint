8361ea26f895bcadda02f67272fa24c36bd7f6d2
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-12-05 21:15:09 +0530

    添加发送站内信代码

--- api-v8/app/Http/Api/WatchApi.php
+++ api-v8/app/Http/Api/WatchApi.php
@@ -1,10 +1,25 @@
 <?php
 namespace App\Http\Api;
 
-use App\Models\Notification;
+use App\Models\Like;
 
 class WatchApi{
-    public static function change($resId,$title,$message){
+    public static function change($resId,$from,$message){
         //发送站内信
+        $watches = Like::where('type','watch')
+                    ->where('target_id',$resId)
+                    ->get();
+        $notifications = [];
+        foreach ($watches as $key => $watch) {
+            $notifications[] = [
+                'from' => $from,
+                'to' => $watch->user_id,
+                'url' => $row['url'],
+                'content' => $message,
+                'res_type' => $watch->res_type,
+                'res_id' => $watch['res_id'],
+            ];
+
+        }
     }
 }
