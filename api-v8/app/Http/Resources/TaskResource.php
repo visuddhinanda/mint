36856de50b52fde56b146f25f2e3c1d8cc5e0c2f
visuddhinanda <visuddhinanda@gmail.com>
2024-12-06 09:10:57 +0000

    :art: formatting

--- api-v8/app/Http/Resources/TaskResource.php
+++ api-v8/app/Http/Resources/TaskResource.php
@@ -22,40 +22,13 @@ class TaskResource extends JsonResource
      */
     public function toArray($request)
     {
-        /*
-  id: string;
-  title: string;
-  description?: string | null;
-  assignees?: IUser[] | null;
-  assignees_id?: string[] | null;
-  parent?: ITaskData | null;
-  parent_id?: string | null;
-  roles?: string[] | null;
-  executor?: IUser | null;
-  executor_id?: string | null;
-  executor_relation_task?: ITaskData | null;
-  executor_relation_task_id?: string | null;
-  pre_task?: ITaskData | null;
-  pre_task_id?: string | null;
-  is_milestone: boolean;
-  project?: IProject|null;
-  project_id?: string | null;
-  owner?: IStudio;
-  owner_id?: string | null;
-  editor?: IUser;
-  editor_id?: string | null;
-  status?: TTaskStatus;
-  created_at?: string;
-  updated_at?: string;
-  started_at?: string | null;
-  finished_at?: string | null;
-         */
+
         $htmlRender = new MdRender([
             'mode' => 'read',
-            'format'=> 'react',
+            'format' => 'react',
             'footnote' => true,
-            'origin' => $request->get('origin',true),
-            'paragraph' => $request->get('paragraph',false),
+            'origin' => $request->get('origin', true),
+            'paragraph' => $request->get('paragraph', false),
         ]);
         $data = [
             'id' => $this->id,
@@ -73,9 +46,9 @@ class TaskResource extends JsonResource
             'project_id' => $this->project_id,
             'project' => ProjectApi::getById($this->project_id),
             'owner_id' => $this->owner_id,
-            "owner"=> StudioApi::getById($this->owner_id),
+            "owner" => StudioApi::getById($this->owner_id),
             'editor_id' => $this->editor_id,
-            "editor"=> UserApi::getByUuid($this->editor_id),
+            "editor" => UserApi::getByUuid($this->editor_id),
             'order' => $this->order,
             'status' => $this->status,
             'created_at' => $this->created_at,
@@ -83,8 +56,8 @@ class TaskResource extends JsonResource
             'started_at' => $this->started_at,
             'finished_at' => $this->finished_at,
         ];
-        $assignees = TaskAssignee::where('task_id',$this->id)->select('assignee_id')->get();
-        if(count($assignees)>0){
+        $assignees = TaskAssignee::where('task_id', $this->id)->select('assignee_id')->get();
+        if (count($assignees) > 0) {
             $assignees_id = [];
             foreach ($assignees as $key => $value) {
                 $assignees_id[] = $value->assignee_id;
@@ -92,11 +65,11 @@ class TaskResource extends JsonResource
             $data['assignees_id'] = $assignees_id;
             $data['assignees'] = UserApi::getListByUuid($assignees_id);
         }
-        if(!empty($this->description)){
-            $data["html"] = $htmlRender->convert($this->description,[]);
+        if (!empty($this->description)) {
+            $data["html"] = $htmlRender->convert($this->description, []);
         }
 
-        if(Str::isUuid($this->executor_id)){
+        if (Str::isUuid($this->executor_id)) {
             $data["executor"] = UserApi::getByUuid($this->executor_id);
         }
         return $data;
