63f76d6f3a76ca615aa405bd669d821bb122c574
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-12-30 20:54:18 +0530

    :art: formatting

--- api-v8/database/migrations/2023_10_01_074218_create_transfers_table.php
+++ api-v8/database/migrations/2023_10_01_074218_create_transfers_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateTransfersTable extends Migration
 {
@@ -16,12 +17,12 @@ class CreateTransfersTable extends Migration
         Schema::create('transfers', function (Blueprint $table) {
             $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v1mc()'));
             $table->uuid('origin_owner')->index();
-            $table->string('res_type',32)->index();
+            $table->string('res_type', 32)->index();
             $table->uuid('res_id')->index();
             $table->uuid('transferor_id')->index();
             $table->uuid('new_owner')->index();
             $table->uuid('editor_id')->nullable()->index();
-            $table->string('status',32)->index()->default('transferred');
+            $table->string('status', 32)->index()->default('transferred');
             $table->timestamps();
         });
     }
