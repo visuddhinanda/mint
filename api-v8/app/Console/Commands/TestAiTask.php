2f9cd64806fef88c8d7072713a21a7a3f8327b58
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2025-01-31 15:43:38 +0530

    添加第二个测试id

--- api-v8/app/Console/Commands/TestAiTask.php
+++ api-v8/app/Console/Commands/TestAiTask.php
@@ -10,6 +10,7 @@ class TestAiTask extends Command
     /**
      * The name and signature of the console command.
      * php artisan test:ai.task c77af42f-ffb5-48ae-af71-4c32e1c30dab
+     * php artisan test:ai.task 81bd0b28-c7ea-4fc5-902d-0b188ba79d35
      * @var string
      */
     protected $signature = 'test:ai.task {id}';
