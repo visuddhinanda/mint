0dbe817d8212c399ca11282da669f129af90367d
visuddhinanda <visuddhinanda@gmail.com>
2025-02-04 16:47:44 +0000

    add system_prompt & real_name & avatar

--- api-v8/database/migrations/2025_01_27_152548_create_ai_models_table.php
+++ api-v8/database/migrations/2025_01_27_152548_create_ai_models_table.php
@@ -17,10 +17,13 @@ class CreateAiModelsTable extends Migration
             $table->id();
             $table->uuid('uid')->unique();
             $table->string('name', 64)->index();
+            $table->string('real_name', 64)->unique();
+            $table->uuid('avatar',)->nullable()->index();
             $table->text('description')->nullable();
             $table->string('url', 1024)->nullable()->index();
             $table->string('model', 1024)->nullable()->index();
             $table->string('key', 1024)->nullable();
+            $table->text('system_prompt')->nullable();
             $table->string('privacy', 32)->index()->default('private')->comment('隐私性:private|public');
             $table->uuid('owner_id')->index()->comment('任务拥有者:用户或者team-space');
             $table->uuid('editor_id')->index();
