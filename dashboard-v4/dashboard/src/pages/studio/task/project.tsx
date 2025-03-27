68f605a778248d0db1f73552918a1109116d12f0
visuddhinanda <visuddhinanda@gmail.com>
2025-03-27 16:13:47 +0000

    使用 ProjectWithTasks

--- dashboard-v4/dashboard/src/pages/studio/task/project.tsx
+++ dashboard-v4/dashboard/src/pages/studio/task/project.tsx
@@ -1,23 +1,19 @@
 import { useNavigate, useParams } from "react-router-dom";
 
-import Project from "../../../components/task/Project";
-import ProjectTask from "../../../components/task/ProjectTask";
+import ProjectWithTasks from "../../../components/task/ProjectWithTasks";
 
 const Widget = () => {
   const { studioname } = useParams();
   const { projectId } = useParams();
   const navigate = useNavigate();
   return (
-    <>
-      <Project
-        studioName={studioname}
-        projectId={projectId}
-        onSelect={(id: string) => {
-          navigate(`/studio/${studioname}/task/project/${id}`);
-        }}
-      />
-      <ProjectTask studioName={studioname} projectId={projectId} />
-    </>
+    <ProjectWithTasks
+      studioName={studioname}
+      projectId={projectId}
+      onChange={(id: string) => {
+        navigate(`/studio/${studioname}/task/project/${id}`);
+      }}
+    />
   );
 };
 
