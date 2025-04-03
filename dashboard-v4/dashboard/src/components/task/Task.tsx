abfea49afce479c10acade22b3c59d699a45ba35
visuddhinanda <visuddhinanda@gmail.com>
2025-04-03 15:05:40 +0000

    :fire: TaskEdit

--- dashboard-v4/dashboard/src/components/task/Task.tsx
+++ dashboard-v4/dashboard/src/components/task/Task.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { ITaskData } from "../api/task";
 import TaskReader from "./TaskReader";
-import TaskEdit from "./TaskEdit";
 
 interface IWidget {
   taskId?: string;
@@ -9,30 +7,14 @@ interface IWidget {
   onChange?: (task: ITaskData[]) => void;
 }
 const Task = ({ taskId, onLoad, onChange }: IWidget) => {
-  const [isEdit, setIsEdit] = useState(false);
-  const [task, setTask] = useState<ITaskData>();
   return (
     <div>
-      {isEdit ? (
-        <TaskEdit
-          taskId={taskId}
-          onLoad={(data: ITaskData) => {}}
-          onChange={(data: ITaskData) => {
-            onChange && onChange([data]);
-            setTask(data);
-            setIsEdit(false);
-          }}
-        />
-      ) : (
-        <TaskReader
-          taskId={taskId}
-          onChange={(data: ITaskData[]) => {
-            onChange && onChange(data);
-            setTask(data.find((t) => t.id === taskId));
-          }}
-          onEdit={() => setIsEdit(true)}
-        />
-      )}
+      <TaskReader
+        taskId={taskId}
+        onChange={(data: ITaskData[]) => {
+          onChange && onChange(data);
+        }}
+      />
     </div>
   );
 };
